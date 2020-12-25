782e6dd6a15108162a9e4d50e71e55bfdb636ee7
Philip Smolen <philip+git@trade-ideas.com>
2020-12-25 01:09:44 -0800

    Bug fix We were putting new squares on top of existing tiles sometimes.

--- doc/js/AztecTiles.js
+++ doc/js/AztecTiles.js
@@ -24,9 +24,23 @@ function getById(id, ty) {
             ty.name);
     }
 }
+/**
+ * The container that holds all of the tiles.
+ */
 var mainDiv = getById("main", HTMLDivElement);
+/**
+ * The number of cells we can draw across or down.
+ * Use setCellCount() to change this.
+ */
 var cellCount = 6;
+/**
+ * We have hit the bounds of our drawing area.  We need more space.
+ */
 var needResizeSoon = false;
+/**
+ *
+ * @param newValue Resize our grid.
+ */
 function setCellCount(newValue) {
     if (!isFinite(newValue) || newValue < 1 || newValue != Math.floor(newValue)) {
         throw new RangeError("Expecting a positive number, got: " + newValue);
@@ -35,6 +49,9 @@ function setCellCount(newValue) {
     mainDiv.style.setProperty("--cells", newValue.toString());
     needResizeSoon = false;
 }
+/**
+ * Delete all tiles.
+ */
 function clearAll() {
     mainDiv.innerText = "";
 }
@@ -63,25 +80,52 @@ function createTestTiles() {
     createTile("right", 3, 4);
     createTile("bottom", 5, 3);
 }
+function oppositeDirection(direction) {
+    switch (direction) {
+        case "top": return "bottom";
+        case "bottom": return "top";
+        case "left": return "right";
+        case "right": return "left";
+    }
+    throw new Error("wtf");
+}
 function moveTilesOnce() {
     function makeKey(row, column) {
         return row + ":" + column;
     }
+    /**
+     * These are places where we expect to see a tile in at the end of this function.
+     * That includes every place that is currently in use, and every place adjacent to a place that is currently in use.
+     * This algorithm is inefficent.  We make no assumptions about the shape of the board.
+     */
     var possiblePositions = new Map();
+    /**
+     * Put the given value into possiblePositions.
+     * @param row
+     * @param column
+     */
     function savePossiblePosition(row, column) {
         possiblePositions.set(makeKey(row, column), { row: row, column: column });
     }
-    var newPositions = new Map();
+    /**
+     * The position of each tile before the move.
+     * We use this to look for tiles that are crossing each other.
+     * We are only storing one copy of each tile.
+     * The key comes from the top left position of tile.
+     * row, column, and keys all the new position, after the move.
+     */
+    var originalPositions = new Map();
     var toDelete = new Set();
     var tiles = document.querySelectorAll(".tile[data-direction]");
     tiles.forEach(function (element) {
-        var _a;
         if (element instanceof HTMLDivElement) {
             var row = +element.style.getPropertyValue("--row");
             var column = +element.style.getPropertyValue("--column");
+            var direction = element.dataset.direction;
+            var originalKey = makeKey(row, column);
             savePossiblePosition(row, column);
             var secondKey = void 0;
-            switch (element.dataset.direction) {
+            switch (direction) {
                 case "top": {
                     savePossiblePosition(row, column + 1);
                     row--;
@@ -111,30 +155,30 @@ function moveTilesOnce() {
                 }
             }
             var firstKey = makeKey(row, column);
-            var conflict = newPositions.get(firstKey);
-            if ((conflict === null || conflict === void 0 ? void 0 : conflict.element) !== ((_a = newPositions.get(secondKey)) === null || _a === void 0 ? void 0 : _a.element)) {
-                // TODO We are failing here a lot.
-                // Our logic is wrong.  We are checking for two blocks landing on top of one another.
-                // We should be looking for two adjacent blocks trying to swap with each other!
-                throw new Error("wtf");
-            }
-            if (conflict) {
-                toDelete.add(conflict.element);
+            var possibleConflict = originalPositions.get(firstKey);
+            if (possibleConflict && (possibleConflict.direction == oppositeDirection(direction))) {
+                toDelete.add(possibleConflict.element);
                 toDelete.add(element);
-                newPositions.delete(firstKey);
-                newPositions.delete(secondKey);
+                originalPositions.delete(firstKey);
             }
             else {
-                newPositions.set(firstKey, { element: element, row: row, column: column });
-                newPositions.set(secondKey, { element: element, row: row, column: column });
+                originalPositions.set(originalKey, { element: element, direction: direction, keys: [firstKey, secondKey], row: row, column: column });
             }
         }
+    });
+    /**
+     * Where we are planning to move each tile.
+     * We use this to say which positions are free.
+     */
+    var newPositions = new Set();
+    originalPositions.forEach(function (tile) {
+        tile.keys.forEach(function (key) { return newPositions.add(key); });
     });
     toDelete.forEach(function (element) {
         // TODO add animation.
         element.remove();
     });
-    newPositions.forEach(function (item) {
+    originalPositions.forEach(function (item) {
         var style = item.element.style;
         style.setProperty("--row", item.row.toString());
         style.setProperty("--column", item.column.toString());
@@ -142,6 +186,8 @@ function moveTilesOnce() {
             needResizeSoon = true;
         }
     });
+    // We already have a list of all the cells that where in use before this function call.
+    // Now we need to add all of the immediate neighbors.
     Array.from(possiblePositions.values()).forEach(function (location) {
         var row = location.row, column = location.column;
         savePossiblePosition(row - 1, column);
@@ -149,6 +195,11 @@ function moveTilesOnce() {
         savePossiblePosition(row, column - 1);
         savePossiblePosition(row, column + 1);
     });
+    /**
+     * This is the same data as possiblePositions, but it's been sorted.
+     * You walk though these positions like English text, the first row
+     * before the second row, left to right within a row.
+     */
     var possibleBlanks = Array.from(possiblePositions);
     possibleBlanks.sort(function (a, b) {
         var firstCompare = a[1].row - b[1].row;
@@ -159,6 +210,9 @@ function moveTilesOnce() {
             return a[1].column - b[1].column;
         }
     });
+    /**
+     * This says which cells are occupied by a "new" tile.
+     */
     var blankAdded = new Set();
     possibleBlanks.forEach(function (item) {
         var key = item[0];
@@ -168,7 +222,14 @@ function moveTilesOnce() {
             createTile("new", row, column_1);
             [row, row + 1].forEach(function (r) {
                 [column_1, column_1 + 1].forEach(function (c) {
-                    blankAdded.add(makeKey(r, c));
+                    var k = makeKey(r, c);
+                    if (blankAdded.has(k)) {
+                        throw new Error("wtf");
+                    }
+                    if (newPositions.has(k)) {
+                        throw new Error("wtf");
+                    }
+                    blankAdded.add(k);
                 });
             });
         }
@@ -213,7 +274,9 @@ function onReset() {
     clearAll();
     setCellCount(8);
 }
+var undo = function () { console.log("Nothing to undo"); };
 function onForward() {
+    undo = saveState();
     if (needResizeSoon) {
         autoResize();
     }
@@ -227,6 +290,25 @@ function onForward() {
         moveTilesOnce();
     }
     // TODO rescale as needed.
+}
+function saveState() {
+    var savedCellCount = cellCount;
+    var tiles = [];
+    document.querySelectorAll(".tile").forEach(function (element) {
+        var _a;
+        if (!(element instanceof HTMLDivElement)) {
+            throw new Error("wtf");
+        }
+        var direction = ((_a = element.dataset.direction) !== null && _a !== void 0 ? _a : "new");
+        var row = +element.style.getPropertyValue("--row");
+        var column = +element.style.getPropertyValue("--column");
+        tiles.push({ direction: direction, row: row, column: column });
+    });
+    return function () {
+        clearAll();
+        setCellCount(savedCellCount);
+        tiles.forEach(function (tile) { return createTile(tile.direction, tile.row, tile.column); });
+    };
 }
 onReset();
 //# sourceMappingURL=AztecTiles.js.map
--- doc/ts/AztecTiles.ts
+++ doc/ts/AztecTiles.ts
@@ -28,11 +28,26 @@ function getById<T extends Element>(id: string, ty: { new (): T }): T {
   }
 }
 
+/**
+ * The container that holds all of the tiles.
+ */
 const mainDiv = getById("main", HTMLDivElement);
 
+/**
+ * The number of cells we can draw across or down.
+ * Use setCellCount() to change this.
+ */
 let cellCount = 6;
+
+/**
+ * We have hit the bounds of our drawing area.  We need more space.
+ */
 let needResizeSoon = false;
 
+/**
+ * 
+ * @param newValue Resize our grid.
+ */
 function setCellCount(newValue : number) {
   if (!isFinite(newValue) || newValue < 1 || newValue != Math.floor(newValue)) {
     throw new RangeError("Expecting a positive number, got: " + newValue);
@@ -42,10 +57,14 @@ function setCellCount(newValue : number) {
   needResizeSoon = false;
 }
 
+/**
+ * Delete all tiles.
+ */
 function clearAll() {
   mainDiv.innerText = "";
 }
 
+/** The type of a tile. */
 type Direction = "top" | "bottom" | "left" | "right" | "new";
 
 function createTile(direction : Direction, row : number, column : number) {
@@ -75,24 +94,57 @@ function createTestTiles() {
   createTile("bottom", 5, 3);
 }
 
+function oppositeDirection(direction : Direction) : Direction {
+  switch (direction) {
+    case "top" : return "bottom";
+    case "bottom" : return "top";
+    case "left" : return "right";
+    case "right" : return "left";
+  }
+  throw new Error("wtf");
+}
+
 function moveTilesOnce() {
   function makeKey(row : number, column : number) : string {
     return row + ":" + column;
   }
+
+  /**
+   * These are places where we expect to see a tile in at the end of this function.
+   * That includes every place that is currently in use, and every place adjacent to a place that is currently in use.
+   * This algorithm is inefficent.  We make no assumptions about the shape of the board.
+   */
   const possiblePositions = new Map<string, { row: number, column : number }>();
+
+  /**
+   * Put the given value into possiblePositions.
+   * @param row 
+   * @param column 
+   */
   function savePossiblePosition(row : number, column : number) {
     possiblePositions.set(makeKey(row, column), {row, column});
   }
-  const newPositions = new Map<string, { element : HTMLDivElement, row : number, column : number}>();
+
+  /**
+   * The position of each tile before the move.  
+   * We use this to look for tiles that are crossing each other.
+   * We are only storing one copy of each tile.
+   * The key comes from the top left position of tile.
+   * row, column, and keys all the new position, after the move.
+   */
+  const originalPositions = new Map<string, { element : HTMLDivElement, direction : Direction, keys : string[], row: number, column : number }>();
+
   const toDelete = new Set<HTMLDivElement>();
   const tiles = document.querySelectorAll(".tile[data-direction]");
   tiles.forEach(element => {
     if (element instanceof HTMLDivElement) {
       let row = +element.style.getPropertyValue("--row");
       let column = +element.style.getPropertyValue("--column");
+      const direction = element.dataset.direction;
+      const originalKey = makeKey(row, column);
       savePossiblePosition(row, column);
       let secondKey : string;
-      switch (element.dataset.direction) {
+      switch (direction) {
         case "top": {
           savePossiblePosition(row, column + 1);
           row--;
@@ -122,29 +174,31 @@ function moveTilesOnce() {
         }
       }
       const firstKey = makeKey(row, column);
-      const conflict = newPositions.get(firstKey);
-      if (conflict?.element !== newPositions.get(secondKey)?.element) {
-        // TODO We are failing here a lot.
-        // Our logic is wrong.  We are checking for two blocks landing on top of one another.
-        // We should be looking for two adjacent blocks trying to swap with each other!
-        throw new Error("wtf");
-      }
-      if (conflict) {
-        toDelete.add(conflict.element);
+      const possibleConflict = originalPositions.get(firstKey);
+      if (possibleConflict && (possibleConflict.direction == oppositeDirection(direction))) {
+        toDelete.add(possibleConflict.element);
         toDelete.add(element);
-        newPositions.delete(firstKey);
-        newPositions.delete(secondKey);
+        originalPositions.delete(firstKey);
       } else {
-        newPositions.set(firstKey, { element, row, column });
-        newPositions.set(secondKey, { element, row, column });
+        originalPositions.set(originalKey, { element, direction, keys: [firstKey, secondKey], row, column });
       }
     }
   });
+
+  /**
+   * Where we are planning to move each tile.
+   * We use this to say which positions are free.
+   */
+  const newPositions = new Set<string>();
+  originalPositions.forEach(tile => {
+    tile.keys.forEach(key => newPositions.add(key));
+  });
+
   toDelete.forEach(element => {
     // TODO add animation.
     element.remove();
   });
-  newPositions.forEach(item => {
+  originalPositions.forEach(item => {
     const style = item.element.style;
     style.setProperty("--row", item.row.toString());
     style.setProperty("--column", item.column.toString());
@@ -152,6 +206,9 @@ function moveTilesOnce() {
       needResizeSoon = true;
     }
   })
+
+  // We already have a list of all the cells that where in use before this function call.
+  // Now we need to add all of the immediate neighbors.
   Array.from(possiblePositions.values()).forEach(location => {
     const { row, column } = location;
     savePossiblePosition(row - 1, column);
@@ -159,6 +216,12 @@ function moveTilesOnce() {
     savePossiblePosition(row, column - 1);
     savePossiblePosition(row, column + 1);
   });
+
+  /**
+   * This is the same data as possiblePositions, but it's been sorted.
+   * You walk though these positions like English text, the first row
+   * before the second row, left to right within a row.
+   */
   const possibleBlanks = Array.from(possiblePositions);
   possibleBlanks.sort((a, b) => {
     const firstCompare = a[1].row - b[1].row;
@@ -168,6 +231,10 @@ function moveTilesOnce() {
       return a[1].column - b[1].column;
     }
   });
+
+  /**
+   * This says which cells are occupied by a "new" tile.
+   */
   const blankAdded = new Set<string>();
   possibleBlanks.forEach(item => {
     const key = item[0];
@@ -177,7 +244,14 @@ function moveTilesOnce() {
       createTile("new", row, column);
       [row, row+1].forEach(r => {
         [column, column + 1].forEach(c=> {
-          blankAdded.add(makeKey(r, c));
+          const k = makeKey(r, c);
+          if (blankAdded.has(k)) {
+            throw new Error("wtf");
+          }
+          if (newPositions.has(k)) {
+            throw new Error("wtf");
+          }
+          blankAdded.add(k);
         });
       });
     }
@@ -226,7 +300,10 @@ function onReset() {
   setCellCount(8);
 }
 
+let undo = () => { console.log("Nothing to undo"); };
+
 function onForward() {
+  undo = saveState();
   if (needResizeSoon) {
     autoResize();
   } else if (mainDiv.childElementCount == 0) {
@@ -237,6 +314,25 @@ function onForward() {
     moveTilesOnce();
   }
   // TODO rescale as needed.
+}
+
+function saveState() {
+  const savedCellCount = cellCount;
+  const tiles = [] as { direction : Direction, row : number, column: number}[];
+   document.querySelectorAll(".tile").forEach(element => {
+    if (!(element instanceof HTMLDivElement)) {
+      throw new Error("wtf")
+    }
+    const direction = (element.dataset.direction ?? "new") as Direction;
+    const row = +element.style.getPropertyValue("--row");
+    const column = +element.style.getPropertyValue("--column");
+    tiles.push({direction, row, column});
+  });
+  return () => {
+    clearAll();
+    setCellCount(savedCellCount);
+    tiles.forEach(tile => createTile(tile.direction, tile.row, tile.column));
+  };
 }
 
 onReset();
